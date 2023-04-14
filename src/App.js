774df5fc892df1b22190a4261c050b93746d225c
Siddhesh <siddhehsongire@gmail.com>
2023-04-14 17:46:30 +0530

    Added HeadlineCards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <HeadlineCards />
     </div>
   );
 }
